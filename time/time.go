2e2f8e675a0392fdc9dbb9f2cd6ec3e0d253e3e0
Ryan Haley <haley.rc@gmail.com>
2022-12-05 23:26:56 -0500

    Refactor the timestamp used for freeze into a constant
    This makes it a lot easier to compare the string value in tests where
    freezing time makes the most sense.

--- time/time.go
+++ time/time.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+const EpochTime = "2022-09-16T15:02:04Z"
+
 type timeFn func() time.Time
 
 var nowFn timeFn = time.Now
@@ -73,7 +75,7 @@ func (t Time) Value() (driver.Value, error) {
 
 func Freeze() {
 	nowFn = func() time.Time {
-		t, err := time.ParseInLocation(time.RFC3339, "2022-09-16T15:02:04Z", time.UTC)
+		t, err := time.ParseInLocation(time.RFC3339, EpochTime, time.UTC)
 		if err != nil {
 			panic(err)
 		}
